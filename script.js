cc16e8ee55e3732ef5312c3d747e56853b0cdb35
Francesco <ciccioflair78@gmail.com>
2023-10-25 17:39:54 +0200

    "Completed al the logic, nothing else todo except for more styling o new functions"

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const rnd = function () {
     return Math.trunc(Math.random() * 20 + 1); 
 }
 
+let highscore = 0;
 let secretNumber = rnd();
 let score = 20;
 document.querySelector(".flip-card-back").textContent = secretNumber;
@@ -28,16 +29,37 @@ document.querySelector('.check').addEventListener ('click', function () {
     } else if (myGuessNum < 0) {
         textMessage.textContent = "⛔ No negative number...";
     } else if (myGuessNum === secretNumber) {
+        
         textMessage.textContent = "🏆 Correct number!!";
+        debugger;
         document.querySelector(".flip-card-back").classList.add("numberFlip");
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+            document.querySelector(".highscore-desktop").textContent = highscore;
+        }
     } else if (myGuessNum > secretNumber) {
         textMessage.textContent = "📈 Too high!!";
         score--
         document.querySelector(".score").textContent = score;
+        document.querySelector("#score-desktop").textContent = score;
     } else if (myGuessNum < secretNumber) {
         textMessage.textContent = "📉 Too low!!"
         score--
         document.querySelector(".score").textContent = score;
+        document.querySelector("#score-desktop").textContent = score;
     }
 });
+
+document.querySelector("#again").addEventListener('click', function() {
+
+    document.querySelector(".flip-card-back").classList.remove("numberFlip"); // remove class from secret number box
+    score = 20 // reset score;
+    document.querySelector(".score").textContent = score;
+    document.querySelector("#score-desktop").textContent = score;
+    secretNumber = rnd(); // new random secret number;
+    document.querySelector(".guess").value = "";
+
+
+})
 
